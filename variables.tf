1e7fc900569864f12b632c81a5859d67fa96ca32
hp40400 <46974890+hp40400@users.noreply.github.com>
2022-08-18 16:10:00 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,12 +1,12 @@
 variable "aws_vpc_name" {
   type        = string
-  default = "Demo-VPC1"
+  default = "Demo-VPC"
   description = "Name of the VPC"
 }
 
 variable "eks_cluster_name" {
   type        = string
-  default = "Demo-EKS1"
+  default = "Demo-EKS"
   description = "Name of the EKS Cluster"
 }
 
@@ -18,7 +18,7 @@ variable "region" {
 terraform {
   backend "s3" {
     bucket = "demo-infra-eks-state"
-    key    = "infrastructure1/terraform.tfstate"
+    key    = "infrastructure/terraform.tfstate"
     region = "us-west-2"
   }
 }
